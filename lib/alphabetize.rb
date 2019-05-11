04557d1e8d6fdf77f2f8c5a8056889168442e117
Jooyon Won <jyon1204@gmail.com>
2019-05-11 17:54:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
-def alphabetize(arr)
-  binding.pry
+def alphabetize(array)
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  array.sort_by do |words|
+    words.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
