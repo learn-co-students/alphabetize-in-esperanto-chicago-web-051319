6275656f8ae455b9487a381c20c74695cb072c13
Emily R <emilyroulund@gmail.com>
2019-05-11 22:00:04 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" 
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrases|
+    phrases.split("").collect do |letters|
+      ESPERANTO_ALPHABET.index(letters)
+  end
+  end
 end
